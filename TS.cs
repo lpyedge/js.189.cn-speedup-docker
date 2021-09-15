2888174f421ad703bf59d41842337ef987f7c57d
lpyedge <18747080@qq.com>
2021-09-15 18:10:18 +0800

    时间判断错误修复，待测试

--- TS.cs
+++ TS.cs
@@ -16,7 +16,7 @@ namespace JSDXTS
         /// <summary>
         ///     接近可以提速时间 延迟执行秒数
         /// </summary>
-        private const int _speedUpShortInterval = 15;
+        private const int _speedUpShortInterval = 25;
         
         /// <summary>
         ///     接近可以提速时间 延迟执行秒数
@@ -145,6 +145,14 @@ namespace JSDXTS
                     if (dateExpire < DateTime.Now) dateExpire = dateExpire.AddDays(1);
 
 
+                    
+                    // [09-15 14:36:01] 成功提速至 200M/50M (下行/上行)
+                    // 提速到期时间 09-15 16:30
+                    //[09-15 16:30:01] 成功提速至 200M/50M (下行/上行)
+                    // 提速到期时间 09-16 16:30
+                    //获取返回时间设置下次执行时已经过期，造成执行了上面的天数+1的代码出现错误
+                    //dateExpire不再增加2分钟测试看看
+                    
                     if (DateTime.Now >= dateExpire)
                     {
                         LogWait();
@@ -155,8 +163,8 @@ namespace JSDXTS
                     else
                     {
                         LogSuccess(dateExpire.AddMinutes(2));
-                        //鉴于目前系统返回的到期时间不准确，提前了3分钟，这里取到期时间放慢了2分钟
-                        var delay = (int) dateExpire.AddMinutes(2).Subtract(DateTime.Now).TotalSeconds;
+                        //鉴于目前系统返回的到期时间不准确，提前了2-3分钟**
+                        var delay = (int) dateExpire.Subtract(DateTime.Now).TotalSeconds;
                         JobManager.AddJob(DoTaskLong,
                             s => s.ToRunOnceIn(delay).Seconds()
                         );
